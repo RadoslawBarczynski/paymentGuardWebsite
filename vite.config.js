24efaefb08ff338d9ff78b679a4086869641f0d7
Radosław <teammenpl@gmail.com>
2023-08-21 23:00:09 +0200

    t

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define:{
+  VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
+  VITE_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY
+  }
 })
